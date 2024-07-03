c1f0531875e8f3340d9685872ae1bf72c6c3b069
Dan <whatdanman@gmail.com>
2024-07-03 23:55:25 +0300

    chore: configure pages

--- src/pages/posts/ui/SinglePostPage.tsx
+++ src/pages/posts/ui/SinglePostPage.tsx
@@ -1,6 +1,6 @@
 import { Post } from "@/pages/posts/ui/post/Post";
 
-export function PostsPage() {
+export function SinglePostPage() {
   return (
     <div className="flex flex-col bg-secondary p-4 w-content gap-4">
       <Post />
