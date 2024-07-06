78eeae3e10964e83bcfc0caee7f9053d5b1e013f
Dan <whatdanman@gmail.com>
2024-07-06 15:56:00 +0300

    chore: add metadata

--- src/pages/posts/ui/SinglePostPage.tsx
+++ src/pages/posts/ui/SinglePostPage.tsx
@@ -1,8 +1,25 @@
 import { PostWithComments } from "@/pages/posts/ui/post/PostWithComments";
+import Head from "next/head";
 
 export function SinglePostPage() {
   return (
     <div className="flex h-fit flex-col bg-secondary p-4 w-content gap-4">
+      <Head>
+        <title>The Quackit is launched today! — Quackit</title>
+        <meta name="description">
+          {
+            "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
+          }
+        </meta>
+        <meta property="og:title">
+          The Quackit is launched today! — Quackit
+        </meta>
+        <meta property="og:description">
+          {
+            "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
+          }
+        </meta>
+      </Head>
       <PostWithComments
         content={
           "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
