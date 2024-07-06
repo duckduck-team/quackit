a53e0029d398f3eecd4d072760a12fe1c508e0f4
Dan <whatdanman@gmail.com>
2024-07-06 15:06:34 +0300

    feat: report share button

---
+++ src/pages/posts/ui/post/ShareButton.tsx
@@ -0,0 +1,11 @@
+import { PostButton } from "@/pages/posts/ui/post/PostButton";
+import { Share2Icon } from "lucide-react";
+
+export function ShareButton() {
+  return (
+    <PostButton className="w-fit flex p-2 px-3 gap-2 bg-secondary font-medium text-sm text-primary/75 rounded-md">
+      <Share2Icon strokeWidth={3} size={16} />
+      Share
+    </PostButton>
+  );
+}
