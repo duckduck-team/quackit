cc746a4c581abc25da9186cb261ed6e967c1279d
Dan <whatdanman@gmail.com>
2024-07-06 14:55:23 +0300

    fix: errors

--- src/pages/posts/ui/post/PostContent.tsx
+++ src/pages/posts/ui/post/PostContent.tsx
@@ -5,18 +5,20 @@ interface PostContentProps {
 }
 export function PostContent({ title, content, post_id }: PostContentProps) {
   function parseContent(content: string) {
-    let contentSep = content.split("\n");
-    console.log(contentSep);
-    return contentSep.map((cont, index) => (
-      <p key={`${post_id}-${index}`}>{cont ? cont : <br />}</p>
-    ));
+    if (content) {
+      let contentSep = content.split("\n");
+      console.log(contentSep);
+      return contentSep.map((cont, index) => (
+        <p key={`${post_id}-${index}`}>{cont ? cont : <br />}</p>
+      ));
+    }
   }
   return (
     <>
       <h1 className="text-base font-semibold">{title}</h1>
-      <p className="w-full h-fit font-normal text-sm">
+      <div className="w-full h-fit font-normal text-sm">
         {parseContent(content)}
-      </p>
+      </div>
     </>
   );
 }
