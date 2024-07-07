87d6f7489169ddc65c339c295a379e5275c6b02c
Dan <whatdanman@gmail.com>
2024-07-07 12:00:51 +0300

    feat: implement voting with dummy auth

--- src/pages/posts/lib/posts.ts
+++ src/pages/posts/lib/posts.ts
@@ -7,9 +7,15 @@ export interface PostInDB extends Response {
   published_at: string;
   detail?: string;
 }
+
+interface Token extends Response {
+  access_token: string;
+  token_type: string;
+}
 export async function fetchPost(post_id: number) {
   return await fetch(
     `${process.env.NEXT_PUBLIC_API_HOST}/unauthorized/post/${post_id}`,
+    { cache: "no-store" },
   )
     .then((r) => {
       if (r.status === 404) return null;
@@ -18,6 +24,49 @@ export async function fetchPost(post_id: number) {
     .then((post: PostInDB) => post)
     .catch((e) => {
       console.log("Error occurred while fetching the post", e);
+      return null;
+    });
+}
+
+async function dummyAuth() {
+  return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/user/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ username: "demo", password: "123456" }),
+  })
+    .then((r) => {
+      if (r.status != 200) return null;
+      return r.json();
+    })
+    .then((token: Token) => token)
+    .catch((e) => {
+      console.log("Error occurred while trying to authorize", e);
+      return null;
+    });
+}
+
+export async function votePost(post_id: number) {
+  const token = await dummyAuth();
+  if (!token) return null;
+  return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/post/vote`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      post_id: post_id,
+    }),
+  })
+    .then((r) => {
+      if (r.status != 200) return null;
+      return r.json();
+    })
+    .then((post: PostInDB) => post)
+    .catch((e) => {
+      console.log("Error occurred while trying to vote", e);
       return null;
     });
 }
