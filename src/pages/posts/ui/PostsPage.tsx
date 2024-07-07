ab01182878e018e30428d2ed3c17c79cbfda2d8d
Ivan Sannikov <i.sannikov@innopolis.university>
2024-07-07 05:55:50 +0300

    added: fetch from api

--- src/pages/posts/ui/PostsPage.tsx
+++ src/pages/posts/ui/PostsPage.tsx
@@ -1,18 +1,79 @@
+'use client'
 import { Post } from "@/pages/posts/ui/post/Post";
+import { useEffect, useState } from "react";
+
+
+interface Post {
+  post_id: number;
+  user_id: number;
+  title: string;
+  content: string;
+  votes_count: number;
+  published_at: string;
+}
 
 export function PostsPage() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const post_list = [
+    {
+      content: "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru",
+      post_id: 1,
+      published_at: "2024-07-06T10:47:17.483Z",
+      title: "The Quackit is launched today!",
+      user_id: 1,
+      votes_count: 0
+    },
+  ]
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts') //TODO: change for our API
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  
   return (
     <div className="flex flex-col bg-secondary p-4 w-content gap-4">
-      <Post
-        content={
-          "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
-        }
-        post_id={1}
-        published_at={"2024-07-06T10:47:17.483Z"}
-        title={"The Quackit is launched today!"}
-        user_id={1}
-        votes_count={0}
-      />
+      {posts.map((post: Post) => (
+        <Post
+          key={post.post_id}
+          content={post.content}
+          post_id={post.post_id}
+          published_at={formatDate(post.published_at)}
+          title={post.title}
+          user_id={post.user_id}
+          votes_count={post.votes_count}
+        />
+      ))}
     </div>
   );
 }
