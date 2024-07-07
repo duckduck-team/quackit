a03b3c90687338883bcab0ba025e265891f45553
Dan <whatdanman@gmail.com>
2024-07-07 11:13:13 +0300

    chore: integrate page with api

--- src/pages/posts/ui/SinglePostPage.tsx
+++ src/pages/posts/ui/SinglePostPage.tsx
@@ -1,35 +1,44 @@
 import { PostWithComments } from "@/pages/posts/ui/post/PostWithComments";
 import Head from "next/head";
+import { fetchPost } from "@/pages/posts/lib/posts";
 
-export function SinglePostPage() {
-  return (
-    <div className="flex h-fit flex-col bg-secondary p-4 w-content gap-4">
-      <Head>
-        <title>The Quackit is launched today! — Quackit</title>
-        <meta name="description">
-          {
-            "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
-          }
-        </meta>
-        <meta property="og:title">
-          The Quackit is launched today! — Quackit
-        </meta>
-        <meta property="og:description">
-          {
-            "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
-          }
-        </meta>
-      </Head>
-      <PostWithComments
-        content={
-          "We are happy to announce that Quackit is available to all users!\n• Share your content with Quackit community via posts. Try now!\n• Share your content with Quackit community via posts. Try now!\n• Comment on posts and discuss the news with the community\n• Vote to rise the interesting posts to the top\n\nHave an idea or anything to say? Contact us via hello@quackit.ru"
-        }
-        post_id={1}
-        published_at={"2024-07-06T10:47:17.483Z"}
-        title={"The Quackit is launched today!"}
-        user_id={1}
-        votes_count={0}
-      />
-    </div>
-  );
+async function getPost(post_id: number) {
+  return await fetchPost(post_id);
+}
+
+export async function SinglePostPage({ post_id }: { post_id: number }) {
+  const postData = await getPost(post_id);
+
+  if (postData) {
+    return (
+      <div className="flex h-fit flex-col bg-secondary p-4 w-content gap-4">
+        <Head>
+          <title>{postData.title}</title>
+          <meta name="description">{postData.content}</meta>
+          <meta property="og:title">{postData.title}</meta>
+          <meta property="og:description">{postData.content}</meta>
+        </Head>
+        <PostWithComments
+          content={postData.content}
+          post_id={postData.post_id}
+          published_at={postData.published_at}
+          title={postData.title}
+          user_id={postData.user_id}
+          votes_count={postData.votes_count}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex h-fit flex-col bg-secondary p-4 w-content gap-4">
+        <Head>
+          <title>404 — Quackit</title>
+        </Head>
+        <div className="flex w-full h-96 gap-4 flex-col justify-center items-center ">
+          <h1 className="text-4xl font-semibold text-black/50">404</h1>
+          <p className="text-xl text-black/50">Requested post does not exist</p>
+        </div>
+      </div>
+    );
+  }
 }
