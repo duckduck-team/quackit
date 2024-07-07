4a75dae1eee3abd89ff1a1fabc652b1c93cc1473
Dan <whatdanman@gmail.com>
2024-07-07 11:13:03 +0300

    chore: pass post_id to page component

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -1,5 +1,5 @@
 import { SinglePostPage } from "@/pages/posts/ui/SinglePostPage";
 
-export default function Post() {
-  return <SinglePostPage />;
+export default function Post({ params }: { params: { postId: number } }) {
+  return <SinglePostPage post_id={params.postId} />;
 }
