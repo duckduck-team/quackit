2af9d7245f511e65bfa16df09e603504058c3ea4
Dan <whatdanman@gmail.com>
2024-07-09 22:24:39 +0300

    chore: change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+    env: {
+        NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST
+    }
+};
 
 export default nextConfig;
