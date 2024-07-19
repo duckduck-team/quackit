49de7b2380055cbab3fdc4556c795ced08d010ac
Dan <whatdanman@gmail.com>
2024-07-20 00:28:11 +0300

    chore: change create post

--- src/pages/createPost/ui/CreatePostPage.tsx
+++ src/pages/createPost/ui/CreatePostPage.tsx
@@ -4,17 +4,25 @@ import { Card } from "@/shared/ui/card";
 import { Input } from "@/shared/ui/input";
 import { UserData } from "@/pages/createPost/ui/createPost/UserData";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/shared/ui/button";
 import { TextareaWithText } from "@/pages/createPost/ui/createPost/TextareaWithText";
 import { ChooseCategory } from "@/pages/createPost/ui/createPost/ChooseCategory";
 import { ToggleGroupCustom } from "@/pages/createPost/ui/createPost/ToggleGroupCustom";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/shared/ui/tabs";
+import { fetchUser } from "@/pages/posts/lib/users";
+import { redirect } from "next/navigation";
+import { Image } from "lucide-react";
 
-export function CreatePostPage() {
+export function CreatePostPage({ user_id }: { user_id: number }) {
   const [isEditMode, setIsEditMode] = useState(true);
   const [text, setText] = useState("");
   const [fileName, setFileName] = useState("");
+  const [postUser, setPostUser] = useState({
+    username: "",
+    user_id: -1,
+  });
+  const [loading, setLoading] = useState(true);
 
   const handleUploadClick = () => {
     document.getElementById("picture")?.click();
@@ -26,10 +34,24 @@ export function CreatePostPage() {
     }
   };
 
+  useEffect(() => {
+    async function getUser(user_id: number) {
+      const user = await fetchUser(user_id);
+      if (user) {
+        setPostUser(user);
+        setLoading(false);
+      } else {
+        redirect("/login");
+        setLoading(false);
+      }
+    }
+    getUser(user_id);
+  }, []);
+
   return (
     <div className="flex flex-col bg-secondary p-4 w-content gap-4">
       <Card className="flex flex-col w-full bg-background p-4 gap-4">
-        <UserData user_id={1} username={"danmaninc"} />
+        <UserData user_id={postUser.user_id} username={postUser.username} />
         <div className="flex flex-row gap-4 justify-between">
           <Input id="title" placeholder="Post title..." className="w-full" />
           <ChooseCategory />
@@ -66,10 +88,11 @@ export function CreatePostPage() {
         <div className="flex flex-row gap-4 items-center">
           <Button
             type="button"
-            className="px-2 py-1 text-sm"
+            variant={"outline"}
+            className="gap-2 text-sm"
             onClick={handleUploadClick}
           >
-            Upload photo
+            <Image size={16} strokeWidth={2} /> Upload photo
           </Button>
           <Input
             id="picture"
