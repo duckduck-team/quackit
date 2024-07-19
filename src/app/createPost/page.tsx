716d030ca1d10b7d0b5cc8414f0c4faff3eebd16
Dan <whatdanman@gmail.com>
2024-07-19 23:41:42 +0300

    chore: add metadata

--- src/app/createPost/page.tsx
+++ src/app/createPost/page.tsx
@@ -1,5 +1,10 @@
 import { CreatePostPage } from "@/pages/createPost/ui/CreatePostPage";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Quackit | Create post",
+  description: "Share your news with the community in a few seconds.",
+};
 export default function Posts() {
   return <CreatePostPage />;
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,11 @@
 import { PostsPage } from "@/pages/posts/ui/PostsPage";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Quackit | News feed",
+  description:
+    "Discuss with the community latest news and share your opinion with others.",
+};
 export default function Posts() {
   return <PostsPage />;
 }
