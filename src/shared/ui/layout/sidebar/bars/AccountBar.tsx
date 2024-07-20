1142348bb816b864794b36c0943e1e2995e6c636
Dan <whatdanman@gmail.com>
2024-07-20 10:00:30 +0300

    chore: comment sidebar sections

--- src/shared/ui/layout/sidebar/bars/AccountBar.tsx
+++ src/shared/ui/layout/sidebar/bars/AccountBar.tsx
@@ -10,18 +10,18 @@ export function AccountBar() {
       >
         <HomeIcon className="mr-2 h-4 w-4" /> Home
       </Link>
-      <Link
-        href={"/"}
-        className="flex gap-4 px-4 py-3 w-[23rem] items-center text-sm"
-      >
-        <UserIcon className="mr-2 h-4 w-4" /> My account
-      </Link>
-      <Link
-        href={"/"}
-        className="flex gap-4 px-4 py-3 w-[23rem] items-center text-sm"
-      >
-        <MessageSquareIcon className="mr-2 h-4 w-4" /> My conversations
-      </Link>
+      {/*<Link*/}
+      {/*  href={"/"}*/}
+      {/*  className="flex gap-4 px-4 py-3 w-[23rem] items-center text-sm"*/}
+      {/*>*/}
+      {/*  <UserIcon className="mr-2 h-4 w-4" /> My account*/}
+      {/*</Link>*/}
+      {/*<Link*/}
+      {/*  href={"/"}*/}
+      {/*  className="flex gap-4 px-4 py-3 w-[23rem] items-center text-sm"*/}
+      {/*>*/}
+      {/*  <MessageSquareIcon className="mr-2 h-4 w-4" /> My conversations*/}
+      {/*</Link>*/}
     </div>
   );
 }
