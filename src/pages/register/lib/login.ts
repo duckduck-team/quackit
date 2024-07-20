6f689e44646db47573be2ae5be57a61068fda2b1
MonitorPC <icowbiyi@gmail.com>
2024-07-20 03:45:41 +0300

    Added: login via API

---
+++ src/pages/register/lib/login.ts
@@ -0,0 +1,22 @@
+import { UserInDB } from "@/pages/posts/lib/users";
+import { Credentials } from "./interfaces";
+import { get_token } from "./token";
+
+export async function login({ username, password }: Credentials) {
+    const token = await get_token(username, password);
+    return token;
+}
+
+export async function current_user(token: string): Promise<UserInDB> {
+    return await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/user/me`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((r) => {
+        return r.json();
+    })
+}
+
--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,10 +1,16 @@
+"use client"
+
 import { Input } from "@/shared/ui/input";
 import { Button } from "@/shared/ui/button";
-import { LogInIcon } from "lucide-react";
+import { LogInIcon, LogOut, User } from "lucide-react";
 import { Sidebar } from "@/shared/ui/layout/sidebar/Sidebar";
 import { PopularAuthors } from "@/shared/ui/layout/popular-authors/PopularAuthors";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { Card } from "../card";
+import { current_user } from "@/pages/register/lib/login";
+import { useEffect, useState } from "react";
+import { UserInDB } from "@/pages/posts/lib/users";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,17 +19,50 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 export function Layout({ children }: LayoutProps) {
+  const [user, setUser] = useState<UserInDB | null>(null);
+
+  useEffect(() => {
+    async function fetch_user(){
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        const user = await current_user(token);
+        setUser(user);
+      }
+    }
+
+    fetch_user()
+          
+  }, []);
+
+  function logout() {
+    localStorage.removeItem('access_token');
+    window.location.reload();
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <header className="flex h-header px-8 py-4 justify-between items-center">
           <span className="text-2xl font-semibold">Quackit</span>
           <Input placeholder="Search Quackit..." className="w-content" />
-          <Link href={`/auth`}>
-            <Button>
-              <LogInIcon className="mr-2 h-4 w-4" /> Login
-            </Button>
-          </Link>
+          {user === null ? (
+            <Link href={`/auth/login`}>
+              <Button>
+                <LogInIcon className="mr-2 h-4 w-4" /> Login
+              </Button>
+            </Link>
+          ):
+            <div className="flex gap-4">
+              <Button>
+                <User className="mr-2 h-4 w-4"/> {user.username}
+              </Button>
+              <Button onClick={logout}>
+                <LogOut className="mr-2 h-4 w-4" /> Logout
+              </Button>
+            </div>
+
+          }
+          
         </header>
         <main className="flex justify-between px-8 pb-4">
           <Sidebar />
